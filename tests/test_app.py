64dffe253863c254a5db8070adf5e4af8bbc4519
Dylan <your email>
2025-11-11 15:12:44 +0000

    Individual space html page. All tests passing

--- tests/test_app.py
+++ tests/test_app.py
@@ -16,3 +16,9 @@ from playwright.sync_api import Page, expect
 
 #     # We assert that it has the text "This is the homepage."
 #     expect(p_tag).to_have_text("This is the homepage.")
+
+def test_get_individual_space(db_connection, page, test_web_address):
+    db_connection.seed("seeds/bubbles_bnb.sql")
+    page.goto(f"http://{test_web_address}/spaces/1")
+    div_tags = page.locator('div')
+    expect(div_tags).to_have_text(['Test_Space_1\nThis is a description of Test_Space_1\n10.0\nTest_User_1'])
