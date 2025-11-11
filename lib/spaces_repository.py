4fd245f0f2e1a04e7b2489b91cdb7b4fb1a41fe4
Dylan <your email>
2025-11-11 11:16:57 +0000

    Spaces repository all method working

---
+++ lib/spaces_repository.py
@@ -0,0 +1,14 @@
+from lib.spaces import Space
+
+class SpaceRepository:
+
+    def __init__(self, connection):
+        self._connection = connection
+
+    def all(self):
+        rows = self._connection.execute('SELECT * from spaces')
+        spaces = []
+        for row in rows:
+            item = Space(row["id"], row["name"], row["description"], row["price_per_night"], row["booked_flag"], row["user_id"])
+            spaces.append(item)
+        return spaces
