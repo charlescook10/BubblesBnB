c52a25929296d6da052dae9fc6f95482138d9b23
Charles Cook <charles.cook80@yahoo.co.uk>
2025-11-11 12:30:16 +0000

    Fixed playwright tests

--- tests/test_spaces_routes.py
+++ tests/test_spaces_routes.py
@@ -11,10 +11,19 @@ def test_get_spaces(db_connection, page, test_web_address):
 
   page.goto(f"http://{test_web_address}/spaces")
 
-  spaces = page.locator('.spaces_listing')
-  expect(spaces).to_have_text([
-    'Test_Space_1', 'This is a description of Test_Space_1',
-    'Test_Space_2', 'This is a description of Test_Space_2',
-    'Test_Space_3', 'This is a description of Test_Space_3',
-    'Test_Space_4', 'This is a description of Test_Space_4' 
+  space_names = page.locator('.space-name')
+  space_descriptions = page.locator('.space-description')
+
+  expect(space_names).to_have_text([
+    'Test_Space_1',
+    'Test_Space_2',
+    'Test_Space_3',
+    'Test_Space_4'
+    ])
+  
+  expect(space_descriptions).to_have_text([
+    'This is a description of Test_Space_1',
+    'This is a description of Test_Space_2',
+    'This is a description of Test_Space_3',
+    'This is a description of Test_Space_4' 
     ])
