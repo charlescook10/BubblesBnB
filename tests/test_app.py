e0f00aa21b1c62048a60271e8a4b345b092bd00e
David Lloyd <deltanet@mac.com>
2025-11-12 10:46:09 +0000

    Updated test_app.py to test approved page contents. See additional comments in file

--- tests/test_app.py
+++ tests/test_app.py
@@ -22,3 +22,17 @@ def test_get_individual_space(db_connection, page, test_web_address):
     page.goto(f"http://{test_web_address}/spaces/1")
     div_tags = page.locator('div')
     expect(div_tags).to_have_text(['Test_Space_1\nThis is a description of Test_Space_1\n10.0\nTest_User_1'])
+
+# Note - above test is failing. The layout of hte page has changed and no longer includes "\n10.0\nTest_User_1".
+# Will discuss at the next meeting
+"""
+GET /approved
+returns the correctly formatted webpage
+"""
+def test_approval_page(db_connection, page, test_web_address):
+    db_connection.seed("seeds/bubbles_bnb.sql")
+    page.goto(f"http://{test_web_address}/approved")
+    heading = page.locator("h1")
+    expect(heading).to_have_text('This booking has been approved')
+    expect(page.get_by_text("Back to all listings")).to_be_visible();
+    expect(page.get_by_role("link", name="Back to all listings"))
