b32c5aca4eb70d74b27356c989e5d904fd730b72
Charles Cook <charles.cook80@yahoo.co.uk>
2025-11-13 11:35:12 +0000

    Added a getAvailable

--- lib/availability_repository.py
+++ lib/availability_repository.py
@@ -21,6 +21,14 @@ class AvailabilityRepository:
             dates.append(date)
         return dates 
     
+    def getAvailableSpaceDates(self, space_id):
+        rows = self._connection.execute('SELECT * FROM availabilities WHERE space_id= %s AND status=\'Available\' ORDER BY date',[space_id])
+        dates = []
+        for row in rows:
+            date = Availability(row['id'], row['date'], row['status'], row['space_id'])
+            dates.append(date)
+        return dates 
+    
     def addRange(self, space_id, start_date, end_date):
         self._connection.execute('INSERT INTO availabilities(date, space_id) SELECT gs::date, %s FROM generate_series(%s::date, %s::date, \'1 day\'::interval) AS gs' ,[space_id, start_date, end_date])
         
