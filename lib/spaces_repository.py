74f93ee57109c0a19c6d3b87f70667defe6e0cc2
Dylan <your email>
2025-11-14 11:54:25 +0000

    Added image adding functionality

--- lib/spaces_repository.py
+++ lib/spaces_repository.py
@@ -10,29 +10,29 @@ class SpaceRepository:
         rows = self._connection.execute('SELECT * from spaces')
         spaces = []
         for row in rows:
-            item = Space(row["id"], row["name"], row["description"], row["price_per_night"], row["booked_flag"], row["user_id"])
+            item = Space(row["id"], row["name"], row["description"], row["price_per_night"], row["booked_flag"], row["image_path"], row["user_id"])
             spaces.append(item)
         return spaces
     
     def find(self, id):
         rows = self._connection.execute('SELECT * from spaces WHERE id = %s', [id])
         row = rows[0]
-        return Space(row["id"], row["name"], row["description"], row["price_per_night"], row["booked_flag"], row["user_id"])
+        return Space(row["id"], row["name"], row["description"], row["price_per_night"], row["booked_flag"], row["image_path"], row["user_id"])
     
     def update(self, space):
-        rows = self._connection.execute('UPDATE spaces SET name=%s, description=%s, price_per_night=%s, booked_flag=%s, user_id=%s WHERE id = %s RETURNING *', [space.name, space.description, space.price_per_night, space.booked_flag, space.user_id, space.id])
+        rows = self._connection.execute('UPDATE spaces SET name=%s, description=%s, price_per_night=%s, booked_flag=%s, image_path=%s, user_id=%s WHERE id = %s RETURNING *', [space.name, space.description, space.price_per_night, space.booked_flag, space.image_path, space.user_id, space.id])
         row = rows[0]
-        return Space(row["id"], row["name"], row["description"], row["price_per_night"], row["booked_flag"], row["user_id"])
+        return Space(row["id"], row["name"], row["description"], row["price_per_night"], row["booked_flag"], row["image_path"], row["user_id"])
     
     
     def add_new_listing(self, space):
         rows = self._connection.execute(
             """
-            INSERT INTO spaces (name, description, price_per_night, booked_flag, user_id)
-            VALUES (%s, %s, %s, %s, %s)
-            RETURNING id, name, description, price_per_night, booked_flag, user_id;
+            INSERT INTO spaces (name, description, price_per_night, booked_flag, image_path, user_id)
+            VALUES (%s, %s, %s, %s, %s, %s)
+            RETURNING id, name, description, price_per_night, booked_flag, image_path, user_id;
             """,
-            [space.name, space.description, space.price_per_night, space.booked_flag, space.user_id]
+            [space.name, space.description, space.price_per_night, space.booked_flag, space.image_path, space.user_id]
         )
 
         row = rows[0]
@@ -42,6 +42,7 @@ class SpaceRepository:
             row["description"],
             row["price_per_night"],
             row["booked_flag"],
+            row["image_path"],
             row["user_id"]
         )
 
@@ -56,7 +57,7 @@ class SpaceRepository:
         rows = self._connection.execute('SELECT * from spaces WHERE user_id = %s', [user_id])
         spaces = []
         for row in rows:
-            item = Space(row["id"], row["name"], row["description"], row["price_per_night"], row["booked_flag"], row["user_id"])
+            item = Space(row["id"], row["name"], row["description"], row["price_per_night"], row["booked_flag"], row["image_path"], row["user_id"])
             spaces.append(item)
         return spaces
     
